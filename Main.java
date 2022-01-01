e2c1c1091dab00c071be3d390ca709c33c310977
Gamesarefun11 <94478022+Gamesarefun11@users.noreply.github.com>
2022-01-01 10:59:49 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ttrtftyrugtihyojnhu xu0ivkgthyunh 8zijaghyu9igboooooooooorjthbfnx8utrjmbfiu tnhjrg");
+        int[] arr = new int[5];
+        for (int i = 0; i < 10; i++) {
+    System.out.println(i);public class Person {
+    public String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}/*
+This is a multi-line comment
+*/Person person = new Person("Judy", 28);
+}
+    }
+}
